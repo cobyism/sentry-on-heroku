96912ea01301f894a2ff1e2ab1bbb722d2d30206
Coby Chapple <coby@cobychapple.com>
2014-09-08 10:02:45 +0100

    set SECRET_KEY to SENTRY_KEY

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,7 @@
 import sentry.conf
 from django.contrib.auth.models import User
 SENTRY_KEY = os.environ.get('SENTRY_KEY')
+SECRET_KEY = SENTRY_KEY
 SENTRY_ADMINS = os.environ.get('ADMINS', '').split(',')
 for admin in SENTRY_ADMINS:
   User.objects.create_superuser(admin, admin, 'passworD1')
