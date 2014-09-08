e5c312e2e9a2367841bbbf229c8e9e0cf86ac0f2
Coby Chapple <coby@cobychapple.com>
2014-09-08 11:18:54 +0100

    remove bootstrap.py

--- bootstrap.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indexlogistics.settings.local")
-
-from django.contrib.auth.models import User
-
-SENTRY_ADMINS = os.environ.get('ADMINS', '').split(',')
-
-for admin in SENTRY_ADMINS:
-  User.objects.create_superuser(admin, admin, 'passworD1')
